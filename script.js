258ffb921e788eb3ac9f1aef2e7ea4ee7dfdc711
Maxwell <81234472+Ma-xwell@users.noreply.github.com>
2021-03-24 00:31:46 +0100

    First message

---
+++ script.js
@@ -0,0 +1,10 @@
+//this is a random message generator
+
+const randomNum = () => {
+    return Math.floor(Math.random()*4)};
+//returns random number from 0 to 4
+
+let messages = ['Have a nice day!']
+
+
+console.log(randomNum());
