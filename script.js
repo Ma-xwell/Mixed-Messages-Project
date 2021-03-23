5c9b0edcba9e1c470f36be4448f9d6788bca0df2
Maxwell <81234472+Ma-xwell@users.noreply.github.com>
2021-03-24 00:43:40 +0100

    All messages added

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //this is a random message generator
 
 const randomNum = () => {
-    return Math.floor(Math.random()*4)};
+    return Math.floor(Math.random()*5)};
 //returns random number from 0 to 4
 
-let messages = ['Have a nice day!']
+let messages = ['Have a nice day!', 'Have you eaten healthy breakfast today?', 
+"Let's start your day with a smile!", 'Well, well...', "You're lucky!"];
+//array that cointans 5 random messages
 
-
-console.log(randomNum());
